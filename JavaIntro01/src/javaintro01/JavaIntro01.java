55be770f3c0145b144af9eb3a50cfa2a737c4f2f
Marcos Rausch <marcosrausch@gmail.com>
2023-06-01 19:42:51 -0300

    first main exercise

--- JavaIntro01/src/javaintro01/JavaIntro01.java
+++ JavaIntro01/src/javaintro01/JavaIntro01.java
@@ -1,21 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos. 
  */
 package javaintro01;
 
 /**
  *
- * @author marco
+ * @author Marcos
  */
 public class JavaIntro01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        short s = 32767;
+        int i = -2147483648;
+        long L = 9223372036854775807L;
+        boolean b = false;
+        char c = '\u0000';
+        String S = "String";
     }
     
 }
