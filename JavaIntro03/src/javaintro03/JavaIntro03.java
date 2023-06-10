1a2150059948201b49c3c251e3bc7079c6bb1113
Marcos Rausch <marcosrausch@gmail.com>
2023-06-10 20:15:47 -0300

    third main excercise

---
+++ JavaIntro03/src/javaintro03/JavaIntro03.java
@@ -0,0 +1,26 @@
+/*
+    Define variables donde puedas alojar los resultados y prueba usar dos
+    operadores de cada tipo.
+ */
+package javaintro03;
+
+/**
+ *
+ * @author Marcos
+ */
+public class JavaIntro03 {
+
+    public static void main(String[] args) {
+        int n1 = 5;
+        int n2 = 2;
+        
+        int sum = n1 + n2;
+        
+        double div = n1 / n2;
+        
+        boolean flag = n2 > n1;
+        
+        n1++;                
+    }
+    
+}
